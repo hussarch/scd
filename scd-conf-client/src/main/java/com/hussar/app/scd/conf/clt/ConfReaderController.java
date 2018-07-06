69d89031623264619ccea3c60f533cd84d978226
萧毅 <xiaoyi@shhxzq.com>
2018-07-06 13:39:54 +0800

    change the conf to conf-server

--- scd-conf-client/src/main/java/com/hussar/app/scd/conf/clt/ConfReaderController.java
+++ scd-conf-client/src/main/java/com/hussar/app/scd/conf/clt/ConfReaderController.java
@@ -1,16 +1,28 @@
 package com.hussar.app.scd.conf.clt;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.CompositePropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.support.StandardServletEnvironment;
+
+import javax.annotation.PostConstruct;
+import java.util.Iterator;
+import java.util.List;
+
 
 /**
  * Created by 肖毅(xiaoyi@shhxzq.com) on 18/7/5.
  */
 @RestController
-public class ConfReaderController {
+public class ConfReaderController{
 
-    @Value("${hello.msg}")
+    //@Value("${hello.msg.xx}")
     private String properValue;
 
     @RequestMapping("/properties")
@@ -18,4 +30,5 @@ public class ConfReaderController {
         return properValue;
     }
 
+    
 }
